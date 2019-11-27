1fb4c647a781d26d0d33e2a02894165024ea17b8
Bilal Sarimeseli <45982392+bilalsarimeseli@users.noreply.github.com>
2019-11-27 14:01:07 -0800

    Updates

---
+++ precache-manifest.700d35e686786a7acebb2518bdc917cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0fcea2973618452b1f9ba0f274c7cbf",
+    "url": "/MyRecipeApp/index.html"
+  },
+  {
+    "revision": "bceded844657dc77349d",
+    "url": "/MyRecipeApp/static/css/main.fe5d6841.chunk.css"
+  },
+  {
+    "revision": "e8677b36b870eadf582b",
+    "url": "/MyRecipeApp/static/js/2.ca25a73a.chunk.js"
+  },
+  {
+    "revision": "bceded844657dc77349d",
+    "url": "/MyRecipeApp/static/js/main.b8e354b7.chunk.js"
+  },
+  {
+    "revision": "58ca16554e60d2c7adf8",
+    "url": "/MyRecipeApp/static/js/runtime-main.269f6886.js"
+  }
+]);
